e6a0c9994f4100083c1be3cfa5a5191153308fc9
lizekss <lekse20@freeuni.edu.ge>
2024-08-17 14:55:29 +0400

    fix targetId errors

--- test_profiler/profile_extensionId.js
+++ test_profiler/profile_extensionId.js
@@ -18,45 +18,46 @@ chrome.action.onClicked.addListener(function (tab) {
         let target;
 
         chrome.debugger.getTargets((result) => {
+            console.log(result)
             target = result.find(t => 
-                t.type === 'worker' && t.title.includes(extensionId)
+                t.title.includes(extensionId)
             );
-        
-        });
 
-        if (target) {
-            console.log("Found target:", target);
-            chrome.debugger.attach({ extensionId: extensionId}, '1.3', async function () {
-                if (chrome.runtime.lastError) {
-                    console.error(chrome.runtime.lastError.message);
-                    return;
-                }
-                console.log('Debugger attached');
-            
-                // Enable debugger
-                chrome.debugger.sendCommand({ extensionId: extensionId}, 'Debugger.enable', () => {
-                    console.log('Debugger enabled');
+            if (target) {
+                const targetId = target.id;
+                console.log("Found target:", target);
+                chrome.debugger.attach({ targetId: targetId}, '1.3', async function () {
+                    if (chrome.runtime.lastError) {
+                        console.error(chrome.runtime.lastError.message);
+                        return;
+                    }
+                    console.log('Debugger attached');
+                
+                    // Enable debugger
+                    chrome.debugger.sendCommand({ targetId: targetId}, 'Debugger.enable', () => {
+                        console.log('Debugger enabled');
+                    });
+        
+                    chrome.debugger.sendCommand({ targetId: targetId }, 'Profiler.enable', () => {
+                        console.log('Profiler enabled');
+                    });
+        
+                    chrome.debugger.sendCommand({ targetId: targetId }, 'Profiler.start', () => {
+                        console.log('Profiler started');
+                    });
+        
+                    //sleep
+                    await new Promise(r => setTimeout(r, 2000));
+        
+                    chrome.debugger.sendCommand({ targetId: targetId }, 'Profiler.stop', (result) => {
+                        console.log('Profiler stopped');
+                        console.log(result.profile);
+                    });
                 });
-    
-                chrome.debugger.sendCommand({ extensionId: extensionId }, 'Profiler.enable', () => {
-                    console.log('Profiler enabled');
-                });
-    
-                chrome.debugger.sendCommand({ extensionId: extensionId }, 'Profiler.start', () => {
-                    console.log('Profiler started');
-                });
-    
-                //sleep
-                await new Promise(r => setTimeout(r, 2000));
-    
-                chrome.debugger.sendCommand({ extensionId: extensionId }, 'Profiler.stop', (result) => {
-                    console.log('Profiler stopped');
-                    console.log(result.profile);
-                });
-            });
-        } else {
-            console.log("No matching target found.");
-        }
+            } else {
+                console.log("No matching target found.");
+            }
+        });
         
     }
 });
