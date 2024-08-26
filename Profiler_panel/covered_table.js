5272be77abda64a1fab4933786e811abc8b6113d
nnaeyno <ngogo20@freeuni.edu.ge>
2024-08-27 00:57:25 +0400

    progress bar not a problem

--- Profiler_panel/covered_table.js
+++ Profiler_panel/covered_table.js
@@ -1,43 +1,43 @@
-// import { Line } from "../progressbar.js";
+import { Line } from "progressbar.js";
 
-// function createProgressBar(containerId, widthPercentage) {
-//   const widthPercent = `${widthPercentage}%`;
-//   var bar = new Line(containerId, {
-//     strokeWidth: 4,
-//     easing: "easeInOut",
-//     duration: 1400,
-//     color: "#FFEA82",
-//     trailColor: "#eee",
-//     trailWidth: 1,
-//     svgStyle: { width: widthPercent, height: "100%" },
-//     text: {
-//       style: {
-//         color: "#999",
-//         position: "absolute",
-//         right: "0",
-//         top: "30px",
-//         padding: 0,
-//         margin: 0,
-//         transform: null,
-//       },
-//       autoStyleContainer: false,
-//     },
-//     from: { color: "#ED6A5A" },
-//     to: { color: "#AFE1AF" },
-//     step: (state, bar) => {
-//       bar.path.setAttribute("stroke", state.color);
-//       bar.setText(widthPercentage + " %");
-//     },
-//   });
+function createProgressBar(containerId, widthPercentage) {
+  const widthPercent = `${widthPercentage}%`;
+  var bar = new Line(containerId, {
+    strokeWidth: 4,
+    easing: "easeInOut",
+    duration: 1400,
+    color: "#FFEA82",
+    trailColor: "#eee",
+    trailWidth: 1,
+    svgStyle: { width: widthPercent, height: "100%" },
+    text: {
+      style: {
+        color: "#999",
+        position: "absolute",
+        right: "0",
+        top: "30px",
+        padding: 0,
+        margin: 0,
+        transform: null,
+      },
+      autoStyleContainer: false,
+    },
+    from: { color: "#ED6A5A" },
+    to: { color: "#AFE1AF" },
+    step: (state, bar) => {
+      bar.path.setAttribute("stroke", state.color);
+      bar.setText(widthPercentage + " %");
+    },
+  });
 
-//   bar.animate(1.0); // Animate from 0.0 to 1.0
-// }
+  bar.animate(1.0); // Animate from 0.0 to 1.0
+}
 
-// export function drawTable(data) {
-//   data.array.forEach((fileName, percentage) => {
-//     let container = document.createElement("div");
-//     container.id = `container${i}`;
-//     document.body.appendChild(container);
-//     createProgressBar(`#${container.id}`, percentage);
-//   });
-// }
+export function drawTable(data) {
+  data.array.forEach((fileName, percentage) => {
+    let container = document.createElement("div");
+    container.id = `container${i}`;
+    document.body.appendChild(container);
+    createProgressBar(`#${container.id}`, percentage);
+  });
+}
