5bdc0545f9dc1a8cf22bed85ea55a9b4fb1562c8
nnaeyno <ngogo20@freeuni.edu.ge>
2024-08-26 13:13:46 +0400

    test ranges

--- test/test.js
+++ test/test.js
@@ -1,11 +1,17 @@
 var assert = require('assert');
 const { countCoveredNumbers } = require('../Profiler_panel/helpers');
 
-describe('Bytes', function () {
-  describe('covered bytes', function () {
-    it('intersecting ranges', function () {
+describe('CoveredBytes', function () {
+  describe('intersecting ranges', function () {
+    it('', function () {
       // 6, 4, 3, 6
       assert.equal(countCoveredNumbers([[10, 15], [5, 12], [18, 20], [25, 30]]), 19);
+    });
+  });
+  describe('independent ranges', function () {
+    it('', function () {
+      // 6, 4, 3, 6
+      assert.equal(countCoveredNumbers([[10, 15], [16, 20], [24, 30]]), 18);
     });
   });
 });
