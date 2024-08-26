147248957e7f519975519290a7fc4a6777d945bf
nnaeyno <ngogo20@freeuni.edu.ge>
2024-08-26 13:18:44 +0400

    basic tests covered

--- test/test.js
+++ test/test.js
@@ -2,15 +2,34 @@ var assert = require('assert');
 const { countCoveredNumbers } = require('../Profiler_panel/helpers');
 
 describe('CoveredBytes', function () {
+  describe('one byte', function () {
+    it('', function () {
+      assert.equal(countCoveredNumbers([[10, 10]]), 1);
+    });
+  });
+  describe('two bytes', function () {
+    it('', function () {
+      assert.equal(countCoveredNumbers([[10, 11]]), 2);
+    });
+  });
+  describe('basic test', function () {
+    it('', function () {
+      assert.equal(countCoveredNumbers([[10, 15]]), 6);
+    });
+  });
+  describe('basic intersect test', function () {
+    it('', function () {
+      assert.equal(countCoveredNumbers([[10, 15], [15, 16]]), 7);
+    });
+  });
   describe('intersecting ranges', function () {
     it('', function () {
-      // 6, 4, 3, 6
-      assert.equal(countCoveredNumbers([[10, 15], [5, 12], [18, 20], [25, 30]]), 19);
+      // 10, 11, 12, 13, 14, 15, 5, 6, 7, 8, 9, 18, 19, 20, 25, 26, 27, 28, 29, 30
+      assert.equal(countCoveredNumbers([[10, 15], [5, 12], [18, 20], [25, 30]]), 20);
     });
   });
   describe('independent ranges', function () {
     it('', function () {
-      // 6, 4, 3, 6
       assert.equal(countCoveredNumbers([[10, 15], [16, 20], [24, 30]]), 18);
     });
   });
